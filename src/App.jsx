72e41866da236554231d08c7f5ae55c68fe6f36e
shaniya-shaju <shaniyashaju123@gmail.com>
2026-07-11 14:54:14 +0530

    View Course

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import AddCourse from './Components/AddCourse'
+import ViewCourse from './Components/ViewCourse'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <AddCourse />
+      <ViewCourse />
     </>
   )
 }
---
+++ src/Components/ViewCourse.jsx
@@ -0,0 +1,81 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+
+const ViewCourse = () => {
+
+    const [data, changeData] = useState([])
+
+    const fetchData = () => {
+
+        axios.get(" https://host-demo-app.onrender.com/api/courses")
+            .then((response) => {
+                changeData(response.data)
+            })
+            .catch()
+
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    return (
+        <div>
+
+            
+
+            <div className="container">
+
+                <h1 className="text-center mt-4">
+                    View All Courses
+                </h1>
+
+                <div className="row mt-4">
+
+                    {
+                        data.map((value, index) => {
+
+                            return (
+
+                                <div
+                                    className="col col-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+                                    key={index}
+                                >
+
+                                    <div className="card shadow h-100">
+
+                                        <div className="card-body">
+
+                                            <h5 className="card-title">
+                                                {value.course_name}
+                                            </h5>
+
+                                            <p className="card-text">
+                                                <strong>Duration:</strong> {value.duration}<br />
+                                                <strong>Fee:</strong> ₹{value.fee}<br />
+                                                <strong>Mode:</strong> {value.mode}<br />
+                                                <strong>Trainer:</strong> {value.trainer}<br />
+                                                <strong>Created At:</strong> {value.created_at}
+                                            </p>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            )
+
+                        })
+                    }
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ViewCourse
